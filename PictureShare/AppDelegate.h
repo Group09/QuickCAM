86ab90fe8a56166b59d4de209935e049f9b6695d
Group09 <sophiavillani@n2-160-37.dhcp.drexel.edu>
2014-06-02 14:36:48 -0400

    Initial Commit

---
+++ PictureShare/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PictureShare
+//
+//  Created by Sophia Villani on 6/2/14.
+//  Copyright (c) 2014 Drexel University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PictureShare/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PictureShare
+//
+//  Created by Sophia Villani on 6/2/14.
+//  Copyright (c) 2014 Drexel University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
